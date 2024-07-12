2b6e070419369c9c84d69baa9faa9332f281609b
Gooble <bivinbheemaiah@gmail.com>
2024-07-12 20:22:27 +0530

    Get required pixels to be generated
    I can add the pixels to the grid but managing grid
    itself is something i need to figure out

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const container = document.querySelector('.container');
+
+
+//get grid size
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+   pixelOnSide = prompt('Enter the number of pixels you need on one side', 16);
+   generatePixels(pixelOnSide)
+});
+
+
+
+//generate grid
+let defaultPixelOnSide = 16;
+generatePixels(defaultPixelOnSide);
+
+
 //pixel generation
-let numberOfPixels = 16*16;
-for(let i = 0; i <numberOfPixels; i++){
-    pixel = document.createElement('div');;
-    container.appendChild(pixel);
+function generatePixels(numberOfPixels){
+    numberOfPixels *= numberOfPixels;
+    for(let i = 0; i <numberOfPixels; i++){
+        pixel = document.createElement('div');;
+        container.appendChild(pixel);
+    }
 }
+
 //color changer
 container.addEventListener('mouseover', changeColor);
 
 function changeColor(e){
     e.target.style.backgroundColor = 'red';
 }
+
+
+
 
