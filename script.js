85885d7b39a05c198631ea075510d85494a41ff5
Gooble <bivinbheemaiah@gmail.com>
2024-07-13 11:27:35 +0530

    Generate proper grids which was broken before
    I searched high and low to find a way to make a
    dynamic grid based on css flexbox alone and turns
    out you just have to use javascript which i had
    already learnt before lmao
    atleast, i spent some time with flexbox(did
    flexbox froggy)

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const body = document.querySelector('body');
 //get grid size
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-   pixelOnSide = prompt('Enter the number of pixels you need on one side', 17);
+   pixelOnSide = Math.round(prompt('Enter the number of pixels you need on one side', 16));
+   if(pixelOnSide > 100 || pixelOnSide < 1){
+        alert("Please enter an integer less than 100 and above 0");
+   }else{
    deletePixels();
    generatePixels(pixelOnSide);
-
+   }
 });
 
 function deletePixels(){
@@ -23,9 +26,12 @@ generatePixels();
 
 //pixel generation
 function generatePixels(numberOfPixels = 16){
+    pixelOnSide = numberOfPixels;
     numberOfPixels *= numberOfPixels;
     for(let i = 0; i <numberOfPixels; i++){
         pixel = document.createElement('div');
+        pixel.style.width = `${480/pixelOnSide}px`;
+        pixel.style.height = `${480/pixelOnSide}px`;
         // pixel.textContent = i;
         container.appendChild(pixel);
     }
